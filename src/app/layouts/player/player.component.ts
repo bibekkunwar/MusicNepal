200c935d72ab4060cd74efc65a4b17b4d089c275
bibekkunwar <bibekkunwar0@gmail.com>
2023-08-02 17:53:32 +0545

    feat: music player design done. rem: audioclip add

--- src/app/layouts/player/player.component.ts
+++ src/app/layouts/player/player.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { AudioService } from 'src/app/services/audioPage/audio.service';
 
 @Component({
   selector: 'app-player',
@@ -6,18 +8,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player.component.css'],
 })
 export class PlayerComponent implements OnInit {
-  playlist: any = {};
-  playLink: any ='https://nplb.truestreamz.com/mnm-audio/713339172637/NOAPC0610280-preview.mp3/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9OC8yLzIwMjMgODoyMzoxNiBBTSZoYXNoX3ZhbHVlPTFsZUpuUG94djZqR2ptS1F2SjNTb3c9PSZ2YWxpZG1pbnV0ZXM9NTAwJmlkPTE=';
-
-  constructor(){}
-  ngOnInit(){
+  audioList: any = [];
+  trackDetail: any;
+  playLink: any = 'https://app.momoappnepal.com/api/v1/musicnepal/albums/duniyako-nimti-eauta_NOAPC0610271';
+activeItem:any;
+previewAudio = "https://app.momoappnepal.com/api/v1/musicnepal/audios/preview?type=track&slug=duniyako-nimti-eauta_NOAPC0610271";
+uniqueSlug: any;
+isPlaying: boolean = false;
+  constructor(
+    private audioService: AudioService,
+  ) {}
+  ngOnInit() {
+    this.fetchPreviewAudio();
 
   }
 
+  fetchPreviewAudio(): void {
+    this.audioService.getPreviewData().subscribe({
+      next: (res) => {
+        this.audioList = res.data.tracks;
+        this.getSpecificTrackDetails(this.audioList[0])
+      },
+      error: (error) => {
+        throw error;
+      },
+    });
+  }
 
+  getSpecificTrackDetails(trackDetail: any) {
+    this.trackDetail = trackDetail;
+    this.activeItem = trackDetail;
+  }
 
+  getaudiotracks(trackDetail:string){
+    this.uniqueSlug = this.trackDetail;
+  }
 
+  playNextTrack(): void {
+    const currentIndex = this.audioList.indexOf(this.activeItem);
+    const nextIndex = (currentIndex + 1) % this.audioList.length;
+    this.getSpecificTrackDetails(this.audioList[nextIndex]);
+  }
 
+  playPreviousTrack(): void {
+    const currentIndex = this.audioList.indexOf(this.activeItem);
+    const previousIndex = (currentIndex - 1 + this.audioList.length) % this.audioList.length;
+    this.getSpecificTrackDetails(this.audioList[previousIndex]);
+  }
+
+  playAudio(): void {
+    // Your audio play logic
+    this.isPlaying = true; // Update the state to playing
+  }
+
+  pauseAudio(): void {
+    // Your audio pause logic
+    this.isPlaying = false; // Update the state to paused
+  }
 
 
 
--- src/app/services/audioPage/audio.service.ts
+++ src/app/services/audioPage/audio.service.ts
@@ -8,14 +8,24 @@ import { Observable } from 'rxjs';
 export class AudioService {
 
 audioUrl = "https://mnm.truestreamz.com/api/v1/musicnepal/genres/albums";
-// previewUrl = "https://nplb.truestreamz.com/mnm-audio/713339172637/NOAPC0610280-preview.mp3/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9OC8yLzIwMjMgODoyMzoxNiBBTSZoYXNoX3ZhbHVlPTFsZUpuUG94djZqR2ptS1F2SjNTb3c9PSZ2YWxpZG1pbnV0ZXM9NTAwJmlkPTE=";
+
+previewUrl = "https://app.momoappnepal.com/api/v1/musicnepal/albums/mrigatrishna_UPC713339172637";
+
+AudioPreviewUrl = 'https://app.momoappnepal.com/api/v1/musicnepal/audios/preview?type=track&slug='; // url for short play of audio
+
 constructor(private http: HttpClient) {}
 
 getAudioData():Observable<any> {
   return this.http.get<any>(this.audioUrl);
 }
 
-// getPreviewData():Observable<any> {
-//   return this.http.get<any>(this.previewUrl)
-// }
+
+
+getPreviewData():Observable<any> {
+  return this.http.get<any>(this.previewUrl)
+}
+
+getPreviewAudio() {
+return this.http.get(this.AudioPreviewUrl)
+}
 }
